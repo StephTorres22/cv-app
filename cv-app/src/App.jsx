c941af154e062db90760687f43198106a7b95b6a
Steph <stazatorres@hotmail.com>
2023-08-18 08:03:25 +0100

    start of forms implementation, display preview and form styling

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import { useState } from "react";
 //import Forms from "./Forms";
-import DetailsDisplay from "./Display";
+import DetailsDisplay from "./components/Display";
 //import person from "./person";
 import ProfessionalExperience from "./ProfessionalExperience";
-import EducationalEstablishment from "./Education";
+import EducationalEstablishment from "./components/Education";
 import Qualification from "./Qualifications";
 import Quali from "./classes/qualification";
+import Establishment from "./classes/establishment";
+import Job from "./classes/job";
 
 function App() {
   const [person, setPerson] = useState({
@@ -19,25 +21,31 @@ function App() {
     establishments: [],
     qualifications: [],
   });
-  const [level, setLevel] = useState("");
-  const [subject, setSubject] = useState("");
-  const [grade, setGrade] = useState("");
-  //const [index, setIndex] = useState("0");
 
- /*  function handleLevelChange(e) {
-    setLevel(e.target.value);
-  } */
+  const defaultValue = "";
+  const [level, setLevel] = useState(defaultValue);
+  const [subject, setSubject] = useState(defaultValue);
+  const [grade, setGrade] = useState(defaultValue);
+  const [estab, setEstab] = useState(defaultValue);
+  const [estabStart, setEstatStart] = useState(defaultValue);
+  const [estabEnd, setEstabEnd] = useState(defaultValue);
+  const [jobPlace, setJobPlace] = useState(defaultValue);
+  const [jobTitle, setJobTitle] = useState(defaultValue);
+  const [jobDescription, setJobDesrciption] = useState(defaultValue);
+  const [jobStart, setJobStart] = useState(defaultValue);
+  const [jobEnd, setJobEnd] = useState(defaultValue);
 
-  function handleSubjectChange(e) {
+  /* keep as anonymous functions unless we need validation */
+  /* function handleSubjectChange(e) {
     setSubject(e.target.value);
   }
 
   function handleGradeChange(e) {
     setGrade(e.target.value);
-  }
+  } */
 
-  function addQualification(e) {
-    e.preventDefault();
+  function addQualification() {
+    //e.preventDefault();
     const updatedDetails = {
       ...person,
       qualifications: person.qualifications
@@ -45,25 +53,86 @@ function App() {
         .reverse(),
     };
     setPerson(updatedDetails);
+    setLevel(defaultValue);
+    setSubject(defaultValue);
+    setGrade(defaultValue);
     console.log(updatedDetails);
   }
 
+  function addEstablishment() {
+    const updatedDetails = {
+      ...person,
+      establishments: person.establishments
+        .toSpliced(0, 0, new Establishment(estab, estabStart, estabEnd))
+        .reverse(),
+    };
+    setPerson(updatedDetails);
+    setEstab(defaultValue);
+    setEstatStart(defaultValue);
+    setEstabEnd(defaultValue);
+    console.log(updatedDetails);
+  }
+
+  function addJob() {
+    const updatedDetails = {
+      ...person,
+      jobs: person.jobs
+        .toSpliced(
+          0,
+          0,
+          new Job(jobPlace, jobTitle, jobDescription, jobStart, jobEnd)
+        )
+        .reverse(),
+    };
+
+    setJobPlace(defaultValue);
+    setJobTitle(defaultValue);
+    setJobDesrciption(defaultValue);
+    setJobStart(defaultValue);
+    setJobEnd(defaultValue);
+    console.log(updatedDetails);
+    setPerson(updatedDetails);
+  }
+
   return (
-    <>
+    <div className="whole">
       <div>
-        <ProfessionalExperience />
-        <EducationalEstablishment />
+        <ProfessionalExperience
+          jobPlace={jobPlace}
+          handlePlaceChange={(e) => setJobPlace(e.target.value)}
+          title={jobTitle}
+          handleTitleChange={(e) => setJobTitle(e.target.value)}
+          description={jobDescription}
+          handleDescriptionChange={(e) => setJobDesrciption(e.target.value)}
+          startDate={jobStart}
+          startChange={(e) => setJobStart(e.target.value)}
+          endDate={jobEnd}
+          endChange={(e) => setJobEnd(e.target.value)}
+          onClick={addJob}
+        />
+        <EducationalEstablishment
+          handleEstablishmentChange={(e) => setEstab(e.target.value)}
+          estabValue={estab}
+          handleClick={addEstablishment}
+          startChange={(e) => setEstatStart(e.target.value)}
+          startDate={estabStart}
+          endChange={(e) => setEstabEnd(e.target.value)}
+          endDate={estabEnd}
+        />
         <Qualification
           levelChange={(e) => setLevel(e.target.value)}
-          subjectChange={handleSubjectChange}
-          gradeChange={handleGradeChange}
+          subjectChange={(e) => setSubject(e.target.value)}
+          gradeChange={(e) => setGrade(e.target.value)}
           onClick={addQualification}
+          levelValue={level}
+          subjectValue={subject}
+          gradeValue={grade}
         />
       </div>
-      <div>
+      <div className="preview">
         <DetailsDisplay obj={person} />
       </div>
-    </>
+    </div>
   );
 }
 
